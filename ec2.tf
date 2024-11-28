52a76d396d2ad35edebebc9f0d6036363c09f4a5
AkshayParsade <adparsade@gmail.com>
2024-11-28 13:06:46 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,19 @@
 provider "aws" {
     region = "us-east-1"
-
   
 }
 
-resource "aws_instance" "instance_1" {
-    ami = ""
+resource "aws_instance" "my_instance" {
+    ami = var.image_id
+    instance_type = "t2.micro"
+    key_name = "N.Vergi_key"
+    security_groups = ["default"]
+}
+
+variable "image_id" {
+    type = string
+    default = "ami-0453ec754f44f9a4a"
+    description = "Enter : AMI ID"
   
 }
+
