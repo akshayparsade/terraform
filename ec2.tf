389835742705fca7f159b7119aac632d0a8d5df1
AkshayParsade <adparsade@gmail.com>
2024-11-28 15:36:08 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -9,20 +9,7 @@ resource "aws_instance" "my_instance" {
     key_name = "N.Vergi_key"
     security_groups = ["default"]
 }
+
+variable "" {
   
-  terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 1.9.8"
-    }
-  }
-
-  required_version = ">= 1.2.0"
 }
-
-provider "aws" {
-    profile = "default"
-}
-
-resource "aws_s3_bucket" "test-bucket-terraf
