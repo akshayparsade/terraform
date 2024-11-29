2ff2ed54f57d4211fec6dea2e7da6ff237300839
AkshayParsade <adparsade@gmail.com>
2024-11-29 18:23:07 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -4,8 +4,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "my_sg" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  sg_name
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
