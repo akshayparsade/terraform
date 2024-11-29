155e7a75ab4c96f71aa3a121b8f4e770e03669a7
AkshayParsade <adparsade@gmail.com>
2024-11-29 18:22:07 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,13 @@ provider "aws" {
   
 }
 
-resource "
+resource resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
 
 resource "aws_instance" "my_instance" {
     ami = var.image_id
