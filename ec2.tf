f31e948c9149d3e95fb7093f27d61564ab5c288c
AkshayParsade <adparsade@gmail.com>
2024-11-29 18:15:35 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "my_instance" {
     instance_type = var.instance_type
     key_name = var.key_name
     #security_groups = var.sg_name
-    vpc_securiy
+    vpc_security_group_ids =
 }    
 
 variable "image_id" { 
