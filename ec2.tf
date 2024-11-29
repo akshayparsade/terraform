40be548d31a28183bc1320f72bd468ad230fbc14
AkshayParsade <adparsade@gmail.com>
2024-11-29 18:05:02 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "my_instance" 
     ami = var.image_id
     instance_type = var.instance_type
     key_name = var.key_name
