7f65d1cdb39e9286598a93e918162a568a2efa4e
AkshayParsade <adparsade@gmail.com>
2024-11-30 16:55:11 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "my_sg" {
 resource "aws_instance" "my_instance" {
     ami = var.image_id
     instance_type = var.instance_type
-    key_name = var.key_name
+    #key_name = var.key_name
     #security_groups = var.sg_name
     vpc_security_group_ids = [aws_security_group.my_sg.id]
     user_data = <<-EOF
