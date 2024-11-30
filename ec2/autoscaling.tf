0871f515795b47d94162bd3ad8a4b515a659f4f8
AkshayParsade <adparsade@gmail.com>
2024-11-30 18:22:10 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_configuration" "lc_home" {
     image_id = var.image_id
-    instance_type = "t2.micro"
+    instance_type = var.
     key_name =
     security_groups =
     user_data = <<-EOF
