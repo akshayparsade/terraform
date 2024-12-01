60ae721b6effd3a28e12f520a6d249a280f9d431
AkshayParsade <adparsade@gmail.com>
2024-12-01 11:56:04 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -69,7 +69,7 @@ resource "aws_autoscaling_group" "asg_home" {
 }
 
 resource "aws_autoscaling_policy" "example" {
-  autoscaling_group_name = "aws_autoscaling_group"
+  autoscaling_group_name = "aws_autoscaling_group.asg_home"
   name                   = "foo"
   policy_type            = "PredictiveScaling"
   predictive_scaling_configuration {
