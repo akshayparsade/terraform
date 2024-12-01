ba43ef8fbfb20edd66dc4cbef65c884f82a268f5
AkshayParsade <adparsade@gmail.com>
2024-12-01 13:34:48 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -92,7 +92,7 @@ resource "aws_autoscaling_group" "asg_laptop" {
   desired_capacity     = 2
   launch_configuration = aws_launch_configuration.lc_laptop
   vpc_zone_identifier  = [var.subnet_id_1, var.subnet_2]
-  target_group_arns = [aws_lb_target_group.tg_laptop.]
+  target_group_arns = [aws_lb_target_group.tg_laptop.arn]
 }
 
 resource "aws_autoscaling_policy" "asg_policy_laptop" {
