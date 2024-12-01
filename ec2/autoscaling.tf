a1d232a4337214cc94c641b1cb844ab892bc3160
AkshayParsade <adparsade@gmail.com>
2024-12-01 11:35:28 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -54,7 +54,7 @@ resource "aws_launch_configuration" "lc_cloth" {
     apt install httpd -y
     systemctl start httpd
     systemctl enable httpd
-    mkdir /var/www/html/laptop
-    echo "<h1> This is laptop Page </h1> <br> <h2> Welcome to laptops </h2> > /var/www/html/laptop/index.html 
+    mkdir /var/www/html/cloth
+    echo "<h1> This is Page </h1> <br> <h2> Welcome to laptops </h2> > /var/www/html/laptop/index.html 
     EOF 
 }
