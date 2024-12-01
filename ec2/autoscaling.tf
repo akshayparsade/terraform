6ade42d1954e01a4bfd40dc54d707e55ff18f655
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:12:36 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -20,7 +20,7 @@ resource "aws_launch_template" "lc_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    security_groups = [aws_security_group.my_sg.id]
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
     user_data = <<-EOF
     #!/bin/bash
     apt install httpd -y
@@ -35,7 +35,7 @@ resource "aws_launch_template" "lc_laptop" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    security_groups = [aws_security_group.my_sg.id]
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
     user_data = <<-EOF
     #!/bin/bash
     apt install httpd -y
@@ -51,7 +51,7 @@ resource "aws_launch_template" "lc_cloth" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    security_groups = [aws_security_group.my_sg.id]
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
     user_data = <<-EOF
     #!/bin/bash
     apt install httpd -y
