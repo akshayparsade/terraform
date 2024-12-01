eac2d22d186c4272413983c8833c8b27f18a3826
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:11:05 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "my_sg"{
 }
 
 #Home
-resource "aws_launch_configuration" "lc_home" {
+resource "aws_launch_template" "lc_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
