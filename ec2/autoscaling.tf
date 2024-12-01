6db192de106f2d46503e760093d8737569abe0eb
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:16:27 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -122,7 +122,7 @@ resource "aws_autoscaling_group" "asg_cloth" {
   min_size             = 2
   desired_capacity     = 2
   launch_template {
-    id = aws_launch_template.lt_home.id
+    id = aws_launch_template.lt_cloth.id
     version = aws_launch_template.lt_cloth.latest_version
   }
   vpc_zone_identifier  = [var.subnet_id_1, var.subnet_2]
