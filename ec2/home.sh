d90a7089a6011c3eab8cbebfb5121a9b0c74e264
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:28:20 +0530

    --- Auto Git Commit ---

---
+++ ec2/home.sh
@@ -0,0 +1,7 @@
+<<-EOF
+    #!/bin/bash
+    apt install httpd -y
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1> Hello World </h1> <br> <h2> Welcome to akkitech.sol </h2> > /var/www/html/index.html 
+    EOF 
