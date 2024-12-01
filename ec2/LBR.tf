0e3ca5a235d48b28bacea6fc80d05fdc4875d6e9
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:06:44 +0530

    --- Auto Git Commit ---

--- ec2/LBR.tf
+++ ec2/LBR.tf
@@ -1,47 +1,48 @@
-
 resource "aws_lb_target_group" "tg_home" {
-  name     = "tg-home"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id = var.vpc_id
-  health_check {
-    path = "/"
+    name = "tg_home"
     port = 80
     protocol = "HTTP"
-  }
+    vpc_id = var.vpc_id
+    health_check {
+        path ="/"
+        port = 80
+        protocol = "HTTP"   
+    }
 }
 
 resource "aws_lb_target_group" "tg_laptop" {
-  name     = "tg-laptop"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id = var.vpc_id
-  health_check {
-    path = "/laptop/"
+    name = "tg_laptop"
     port = 80
     protocol = "HTTP"
-  }
+    vpc_id = var.vpc_id
+    health_check {
+        path ="/laptop/*"
+        port = 80
+        protocol = "HTTP"   
+    }
 }
 
-resource "aws_lb_target_group" "tg_mobile" {
-  name     = "tg-mobile"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id = var.vpc_id
-  health_check {
-    path = "/mobile/"
+resource "aws_lb_target_group" "tg_cloth" {
+    name = "tg_cloth"
     port = 80
     protocol = "HTTP"
-  }
+    vpc_id = var.vpc_id
+    health_check {
+        path ="/cloth/*"
+        port = 80
+        protocol = "HTTP"   
+    }
 }
+
 resource "aws_lb" "my_lb" {
   name               = "my-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.my_sg.id]
   subnets            = [var.subnet_id_1, var.subnet_id_2]
+
   tags = {
-    app = "my-app"
+    app = "my-app" 
     env = "dev"
   }
 }
@@ -56,13 +57,15 @@ resource "aws_lb_listener" "my_lb_listener" {
   }
 }
 
-resource "aws_lb_listener_rule" "my_lb_listener_rule_laptop" {
-  listener_arn = aws_lb_listener.my_lb_listener.arn
+resource "my_lb_listener_rule" "my_lb_listener_rule_laptop" {
+  listener_arn = my_lb_listener.my_lb_listene.arn
   priority     = 100
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg_laptop.arn
   }
+
   condition {
     path_pattern {
       values = ["/laptop*"]
@@ -70,16 +73,18 @@ resource "aws_lb_listener_rule" "my_lb_listener_rule_laptop" {
   }
 }
 
-resource "aws_lb_listener_rule" "my_lb_listener_rule_mobile" {
-  listener_arn = aws_lb_listener.my_lb_listener.arn
+resource "my_lb_listener_rule" "my_lb_listener_cloth" {
+  listener_arn = my_lb_listener.my_lb_listene.arn
   priority     = 101
+
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_mobile.arn
+    target_group_arn = aws_lb_target_group.tg_cloth.arn
   }
+
   condition {
     path_pattern {
-      values = ["/mobile*"]
+      values = ["/cloth*"]
     }
   }
 }
