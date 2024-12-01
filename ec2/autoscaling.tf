86acc96c8713d2d85232eb5ebeccd3211427f452
AkshayParsade <adparsade@gmail.com>
2024-12-01 11:34:28 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -40,6 +40,6 @@ resource "aws_launch_configuration" "lc_laptop" {
     systemctl start httpd
     systemctl enable httpd
     mkdir /var/www/html/laptop
-    echo "<h1> This is laptop Page </h1> <br> <h2> Welcome to </h2> > /var/www/html/index.html 
+    echo "<h1> This is laptop Page </h1> <br> <h2> Welcome to laptops </h2> > /var/www/html/index.html 
     EOF 
 }
