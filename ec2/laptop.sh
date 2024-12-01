91e6261713fb7a914f49eb5ef5bacd8c647a284d
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:30:10 +0530

    --- Auto Git Commit ---

--- ec2/laptop.sh
+++ ec2/laptop.sh
@@ -1,8 +1,8 @@
-<<-EOF
+
     #!/bin/bash
     apt install httpd -y
     systemctl start httpd
     systemctl enable httpd
     mkdir /var/www/html/laptop
     echo "<h1> This is laptop Page </h1> <br> <h2> Welcome to laptops </h2> > /var/www/html/laptop/index.html 
-    EOF 
+ 
