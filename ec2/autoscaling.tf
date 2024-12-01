31b10b232f9ea80ebbdf86b9d095bd13ce545d88
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:31:41 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -30,7 +30,7 @@ resource "aws_launch_template" "lc_laptop" {
     instance_type = var.instance_type
     key_name = var.key_pair
     vpc_security_group_ids = [aws_security_group.my_sg.id]
-    user_data = user_data = filebase64("./home.sh")
+    user_data = filebase64("./laptop.sh")
 }
 }
 
