abac3ae2a34660a9d2ab9eb797354193b395fd0b
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:29:00 +0530

    --- Auto Git Commit ---

---
+++ ec2/cloth.sh
@@ -0,0 +1,8 @@
+<<-EOF
+    #!/bin/bash
+    apt install httpd -y
+    systemctl start httpd
+    systemctl enable httpd
+    mkdir /var/www/html/cloth
+    echo "<h1> This is Clothing Page </h1> <br> <h2> SALE ! SALE ! SALE !! </h2> > /var/www/html/cloth/index.html 
+    EOF 
