064e4705b93aec8919ea1d421447b01258c7ac34
AkshayParsade <adparsade@gmail.com>
2024-12-01 11:30:37 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -19,7 +19,7 @@ resource "aws_launch_configuration" "lc_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    security_groups = var.
+    security_groups = var.my_sg
     user_data = <<-EOF
     #!/bin/bash
     apt install httpd -y
