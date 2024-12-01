fc8ada236649da676603e80bc6feef55efc1f2d6
AkshayParsade <adparsade@gmail.com>
2024-12-01 13:48:11 +0530

    --- Auto Git Commit ---

--- ec2/LBR.tf
+++ ec2/LBR.tf
@@ -14,6 +14,7 @@ resource "aws_lb_target_group" "tg_laptop" {
     name = "tg_laptop"
     port = 80
     protocol = "HTTP"
+    vpc_id = var.vpc_id
     health_check {
         path ="/laptop/"
         port = 80
@@ -25,6 +26,7 @@ resource "aws_lb_target_group" "tg_cloth" {
     name = "tg_cloth"
     port = 80
     protocol = "HTTP"
+    vpc_id = var.vpc_id
     health_check {
         path ="/cloth/"
         port = 80
