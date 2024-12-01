6b2c66d2699bb6eff5d82d4d6b4f6b8442ff5ddd
AkshayParsade <adparsade@gmail.com>
2024-12-01 11:30:07 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -19,7 +19,7 @@ resource "aws_launch_configuration" "lc_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    security_groups = var.
+    security_groups = var.https://github.com/ashokitschool/kubernetes_statefulset_ymls.git
     user_data = <<-EOF
     #!/bin/bash
     apt install httpd -y
