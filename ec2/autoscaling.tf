240e726904f86e916540d5bb3b12359dac2b2156
AkshayParsade <adparsade@gmail.com>
2024-12-01 18:06:24 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -1,64 +1,79 @@
-resource "aws_security_group" "my_sg" {
+resource "aws_security_group" "my_sg"{
     name = "allow-http-public"
-    description = "allow HTTP Port"
+    description ="allow HTTP Port"
     ingress {
-        from_port        = 80
-        to_port          = 80
-        protocol         = "TCP"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port  = 80
+        to_port    = 80
+        protocol   = "TCP"
+        cidr_blocks = ["0.0.0.0/0"] 
     }
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol ="-1"
+        cidr_blocks =["0.0.0.0/0"]
     }
 }
 
-resource "aws_launch_template" "lt_home" {
+#Home
+resource "aws_launch_configuration" "lc_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    vpc_security_group_ids = [aws_security_group.my_sg.id]
-    user_data = filebase64("./home.sh")
+    security_groups = [aws_security_group.my_sg.id]
+    user_data = <<-EOF
+    #!/bin/bash
+    apt install httpd -y
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1> Hello World </h1> <br> <h2> Welcome to akkitech.sol </h2> > /var/www/html/index.html 
+    EOF 
 }
 
-resource "aws_launch_template" "lt_laptop" {
+#laptop
+resource "aws_launch_configuration" "lc_laptop" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    vpc_security_group_ids = [aws_security_group.my_sg.id]
-    user_data = filebase64("./laptop.sh")
+    security_groups = [aws_security_group.my_sg.id]
+    user_data = <<-EOF
+    #!/bin/bash
+    apt install httpd -y
+    systemctl start httpd
+    systemctl enable httpd
+    mkdir /var/www/html/laptop
+    echo "<h1> This is laptop Page </h1> <br> <h2> Welcome to laptops </h2> > /var/www/html/laptop/index.html 
+    EOF 
 }
 
-resource "aws_launch_template" "lt_mobile" {
+#cloth
+resource "aws_launch_configuration" "lc_cloth" {
     image_id = var.image_id
     instance_type = var.instance_type
     key_name = var.key_pair
-    vpc_security_group_ids = [aws_security_group.my_sg.id]
-    user_data = filebase64("./mobile.sh")
+    security_groups = [aws_security_group.my_sg.id]
+    user_data = <<-EOF
+    #!/bin/bash
+    apt install httpd -y
+    systemctl start httpd
+    systemctl enable httpd
+    mkdir /var/www/html/cloth
+    echo "<h1> This is Clothing Page </h1> <br> <h2> SALE ! SALE ! SALE !! </h2> > /var/www/html/cloth/index.html 
+    EOF 
 }
-
-# name
-# launch configuration
-# group size
-# sclaing policy (condition (CloudWatch - Alarm), action)
 
 resource "aws_autoscaling_group" "asg_home" {
-  name                 = "asg-home"
-  max_size             = 5
+  name                 = "asg_home"
+  max_size             = 4
   min_size             = 2
   desired_capacity     = 2
-  launch_template {
-    id = aws_launch_template.lt_home.id
-    version = aws_launch_template.lt_home.latest_version
-  }
-  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_id_2]
+  launch_configuration = aws_launch_configuration.lc_home
+  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_2]
   target_group_arns = [aws_lb_target_group.tg_home.arn]
 }
 
 resource "aws_autoscaling_policy" "asg_policy_home" {
-  autoscaling_group_name = aws_autoscaling_group.asg_home.name
+  autoscaling_group_name = "aws_autoscaling_group.asg_home.name"
   name                   = "asg_policy_home"
   policy_type            = "PredictiveScaling"
   predictive_scaling_configuration {
@@ -72,20 +87,17 @@ resource "aws_autoscaling_policy" "asg_policy_home" {
 }
 
 resource "aws_autoscaling_group" "asg_laptop" {
-  name                 = "asg-laptop"
-  max_size             = 5
+  name                 = "asg_laptop"
+  max_size             = 4
   min_size             = 2
   desired_capacity     = 2
-  launch_template {
-    id = aws_launch_template.lt_laptop.id
-    version = aws_launch_template.lt_laptop.latest_version
-  }  
-  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_id_2]
+  launch_configuration = aws_launch_configuration.lc_laptop
+  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_2]
   target_group_arns = [aws_lb_target_group.tg_laptop.arn]
 }
 
 resource "aws_autoscaling_policy" "asg_policy_laptop" {
-  autoscaling_group_name = aws_autoscaling_group.asg_laptop.name
+  autoscaling_group_name = "aws_autoscaling_group.asg_laptop.name"
   name                   = "asg_policy_laptop"
   policy_type            = "PredictiveScaling"
   predictive_scaling_configuration {
@@ -98,22 +110,20 @@ resource "aws_autoscaling_policy" "asg_policy_laptop" {
   }
 }
 
-resource "aws_autoscaling_group" "asg_mobile" {
-  name                 = "asg-mobile"
-  max_size             = 5
+resource "aws_autoscaling_group" "asg_cloth" {
+  name                 = "asg_cloth"
+  max_size             = 4
   min_size             = 2
   desired_capacity     = 2
-  launch_template {
-    id = aws_launch_template.lt_mobile.id
-    version = aws_launch_template.lt_mobile.latest_version
-  }  
-  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_id_2]
-  target_group_arns = [aws_lb_target_group.tg_mobile.arn]
+  launch_configuration = aws_launch_configuration.lc_cloth
+  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_2]
+  target_group_arns = [aws_lb_target_group.tg_cloth.arn]
+
 }
 
-resource "aws_autoscaling_policy" "asg_policy_mobile" {
-  autoscaling_group_name = aws_autoscaling_group.asg_mobile.name
-  name                   = "asg_policy_mobile"
+resource "aws_autoscaling_policy" "asg_policy_cloth" {
+  autoscaling_group_name = "aws_autoscaling_group.asg_home.cloth"
+  name                   = "asg_policy_cloth"
   policy_type            = "PredictiveScaling"
   predictive_scaling_configuration {
     metric_specification {
