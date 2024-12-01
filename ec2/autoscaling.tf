6936ed7153034f60546a7b8263ba4ba6497c235e
AkshayParsade <adparsade@gmail.com>
2024-12-01 11:31:38 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -28,3 +28,17 @@ resource "aws_launch_configuration" "lc_home" {
     echo "<h1> Hello World </h1> <br> <h2> Welcome to akkitech.sol </h2> > /var/www/html/index.html 
     EOF 
 }
+
+resource "aws_launch_configuration" "lc_home" {
+    image_id = var.image_id
+    instance_type = var.instance_type
+    key_name = var.key_pair
+    security_groups = aws_security_group.my_sg.id
+    user_data = <<-EOF
+    #!/bin/bash
+    apt install httpd -y
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1> Hello World </h1> <br> <h2> Welcome to akkitech.sol </h2> > /var/www/html/index.html 
+    EOF 
+}
