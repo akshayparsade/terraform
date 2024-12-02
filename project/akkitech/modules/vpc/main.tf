91a8e264143868349cd37b5f803254c162e51319
AkshayParsade <adparsade@gmail.com>
2024-12-02 14:02:43 +0530

    --- Auto Git Commit ---

--- project/akkitech/modules/vpc/main.tf
+++ project/akkitech/modules/vpc/main.tf
@@ -38,7 +38,6 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_default_route_table" "r" {
   default_route_table_id = aws_vpc.my_vpc.default_route_table_id
-
   route {
     cidr_block = "0.0.0.0/0"
     gatway_id = aws_internet_gateway.igw.id
