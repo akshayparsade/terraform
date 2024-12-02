6ade5c51d94e1a63c57077ce0d845722df3d5e20
AkshayParsade <adparsade@gmail.com>
2024-12-02 12:48:33 +0530

    --- Auto Git Commit ---

--- ec2/akkitech/main.tf
+++ ec2/akkitech/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.private_subnet_cidr
   tags = {
