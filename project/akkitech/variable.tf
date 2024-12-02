cdb5f21d9cd1a0813abb3aae31271fc953a02ff3
AkshayParsade <adparsade@gmail.com>
2024-12-02 14:23:28 +0530

    --- Auto Git Commit ---

--- project/akkitech/variable.tf
+++ project/akkitech/variable.tf
@@ -20,3 +20,11 @@ variable "pub_cidr" {
 variable = "image_id" {
     default = "ami-0453ec754f44f9a4a"
 }
+
+variable = "instance_type" {
+    default = "ami-0453ec754f44f9a4a"
+}
+
+variable = "image_id" {
+    default = "ami-0453ec754f44f9a4a"
+}
