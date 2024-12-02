1985c3caab716369dd3141bd19559a4d75c1cdd0
AkshayParsade <adparsade@gmail.com>
2024-12-02 13:15:31 +0530

    --- Auto Git Commit ---

--- ec2/akkitech/modules/ec2/main.tf
+++ ec2/akkitech/modules/ec2/main.tf
@@ -4,3 +4,10 @@ resource "aws_instance" "private_instance" {
     vpc_security_group_ids = var.vpc_security_group_id
     key_name = var.key_pair
 }
+
+resource "aws_instance" "public_instance" {
+    ami = var.image_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.vpc_security_group_id
+    key_name = var.key_pair
+}
