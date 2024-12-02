3e795e710438bed211244faf1647122bed2302fd
AkshayParsade <adparsade@gmail.com>
2024-12-02 13:05:48 +0530

    --- Auto Git Commit ---

--- ec2/akkitech/modules/vpc/main.tf
+++ ec2/akkitech/modules/vpc/main.tf
@@ -40,7 +40,8 @@ resource "aws_default_route_table" "rt" {
   default_route_table_id = aws_vpc.my_vpc.default_route_table_id
 
   route {
-    cidr_block = ""
+    cidr_block = "0.0.0.0/0"
+    ga
   }
 
   tags = {
