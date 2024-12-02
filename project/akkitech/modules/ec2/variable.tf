699986c3fd82d2f403446da9bd51d3d7b8e2fa6b
AkshayParsade <adparsade@gmail.com>
2024-12-02 14:22:08 +0530

    --- Auto Git Commit ---

--- project/akkitech/modules/ec2/variable.tf
+++ project/akkitech/modules/ec2/variable.tf
@@ -1,10 +1,10 @@
   variable "image_id" {
-    default = "ami-0453ec754f44f9a4a"
+    default =
   }
 
   variable "instance_type" {
-    default = "t2.micro"
-  } 
+    default =
+  }
 
   variable "security_group_id" {
     default =
