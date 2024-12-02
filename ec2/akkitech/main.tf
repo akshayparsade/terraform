83d5fa80d4bb1c4790df23408da00cb153c27d48
AkshayParsade <adparsade@gmail.com>
2024-12-02 12:47:23 +0530

    --- Auto Git Commit ---

--- ec2/akkitech/main.tf
+++ ec2/akkitech/main.tf
@@ -11,8 +11,6 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.private_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
   tags = {
     Name = "PublicSubnet"
   }
