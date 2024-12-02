0d74ab3ec11da5bc0aed90621c5927d53e1ce72c
AkshayParsade <adparsade@gmail.com>
2024-12-02 17:16:57 +0530

    --- Auto Git Commit ---

--- project/akkitech/modules/vpc/output.tf
+++ project/akkitech/modules/vpc/output.tf
@@ -1,11 +1,11 @@
 output "private_subnet_id" {
     value = aws_subnet.private_subnet.id
-}
+} 
 
 output "public_subnet_id" {
     value = aws_subnet.public_subnet.id
 }
 
 output "vpc_id" {
-    value = aws_vpc.my_vpc.id
-}
+    value = aws_vpc.cbz_vpc.id
+} 
