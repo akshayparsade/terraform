01c7542fd8ecafdd3840a4eb1cf624ef0338cabd
AkshayParsade <adparsade@gmail.com>
2024-12-02 18:31:04 +0530

    --- Auto Git Commit ---

--- project/akkitech/main.tf
+++ project/akkitech/main.tf
@@ -1,4 +1,11 @@
-?
+terraform {
+  required_version = ">= 1.0"
+  backend "s3" {
+    bucket = "akkitech-tf-bucket"
+    region = "us-east-1"
+    key = "tfstate"
+  }
+}
 provider "aws" {
     region = "us-east-1"
 }
