b589df33b1f344e971189fe2e04e2d23675e9e34
AkshayParsade <adparsade@gmail.com>
2024-12-02 17:16:16 +0530

    --- Auto Git Commit ---

--- project/akkitech/modules/vpc/main.tf
+++ project/akkitech/modules/vpc/main.tf
@@ -1,36 +1,33 @@
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block     = "10.0.0.0/16"
+resource "aws_vpc" "cbz_vpc" {
+  cidr_block       = var.cidr
   tags = {
-    Name = "${var.project}-VPC"
-    env = var.env
-  }
-}
-
-# Create Subnets
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.project}-public_subnet"
+    Name = "${var.project}-vpc"
     env = var.env
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block      = var.private_subnet_cidr
-  
+  vpc_id     = aws_vpc.cbz_vpc.id
+  cidr_block = var.private_subnet_cidr
+
   tags = {
-    Name = "${var.project}-private_subnet"
+    Name = "${var.project}-private-subnet"
+    env = var.env
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.cbz_vpc.id
+  cidr_block = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project}-public-subnet"
     env = var.env
   }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
+  vpc_id = aws_vpc.cbz_vpc.id
   tags = {
     Name = "${var.project}-igw"
     env = var.env
@@ -38,13 +35,15 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_default_route_table" "r" {
-  default_route_table_id = aws_vpc.my_vpc.default_route_table_id
+  default_route_table_id = aws_vpc.cbz_vpc.default_route_table_id 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    Name = "${var.project}-default_rt"
+    Name = "${var.project}-default-rt"
     env = var.env
   }
 }
+
+ 
