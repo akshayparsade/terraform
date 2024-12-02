e628a5fbdc590176eeb43d24856ed17cfe5e98af
AkshayParsade <adparsade@gmail.com>
2024-12-02 14:06:14 +0530

    --- Auto Git Commit ---

--- project/akkitech/modules/vpc/main.tf
+++ project/akkitech/modules/vpc/main.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.project}-public_subnet"
     env = var.env
