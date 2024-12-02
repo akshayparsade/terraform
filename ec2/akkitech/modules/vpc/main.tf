490de09810c12786c4678ff839b3b1a1a452b130
AkshayParsade <adparsade@gmail.com>
2024-12-02 12:56:06 +0530

    --- Auto Git Commit ---

---
+++ ec2/akkitech/modules/vpc/main.tf
@@ -0,0 +1,27 @@
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  tags = {
+    Name = "${var.project}-VPC"
+    env = var.env
+  }
+}
+
+# Create Subnets
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  tags = {
+    Name = "${var.project}-public_subnet"
+    env = var.env
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  tags = {
+    Name = "${var.project}-private_subnet"
+    env = var.env
+  }
+}
