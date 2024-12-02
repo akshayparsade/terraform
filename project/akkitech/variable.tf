0bc60f624df9ef0b849431a687691f2b424eb385
AkshayParsade <adparsade@gmail.com>
2024-12-02 17:19:48 +0530

    --- Auto Git Commit ---

--- project/akkitech/variable.tf
+++ project/akkitech/variable.tf
@@ -1,31 +1,31 @@
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+    default = "192.168.0.0/16"
 }
 
 variable "project_name" {
-    default = "akkitech"
+    default = "cbz"
 }
 
 variable "env" {
     default = "dev"
 }
+
 variable "pri_cidr" {
-    default ="10.0.0.0/20"
+    default = "192.168.0.0/20"
 }
 
 variable "pub_cidr" {
-   default = "10.0.16.0/20"
-}     
+    default = "192.168.16.0/20"
+}
 
 variable "image_id" {
     default = "ami-012967cc5a8c9f891"
 }
 
-
 variable "instance_type" {
     default = "t2.micro"
 }
 
-variable = "login-key" {
-    default = "N.Vergi_key"
+variable "login_key" {
+    default = "new-key-nv"
 }
