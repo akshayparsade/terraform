b2c9671321c5fc0563aa4ed9f8ce5f54d5946d6f
AkshayParsade <adparsade@gmail.com>
2024-12-03 16:45:30 +0530

    --- Auto Git Commit ---

--- IAM/main.tf
+++ IAM/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # Change to your preferred region
 }
 
-resource "aws_iam_user" "Dev" {
+resource "aws_iam_user" "example_user" {
   name = "example-user"
 }
 
