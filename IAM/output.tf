8538c0644facef8b2c927bbadd25a7b026f6dc46
AkshayParsade <adparsade@gmail.com>
2024-12-03 16:50:11 +0530

    --- Auto Git Commit ---

---
+++ IAM/output.tf
@@ -0,0 +1,13 @@
+
+output "access_key_id" {
+  value = aws_iam_access_key.Dev_access_key.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.Dev_access_key.secret
+  sensitive = true
+}
+
+output "console_login_url" {
+  value = "https://console.aws.amazon.com/"
+}
