6fbab427cb254ce42e0a94a47a158eb1b7340f9b
AkshayParsade <adparsade@gmail.com>
2024-12-03 16:44:20 +0530

    --- Auto Git Commit ---

---
+++ IAM/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1" # Change to your preferred region
+}
+
+resource "aws_iam_user" "example_user" {
+  name = "example-user"
+}
+
+resource "aws_iam_user_policy_attachment" "s3_full_access" {
+  user       = aws_iam_user.example_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_access_key" "example_access_key" {
+  user = aws_iam_user.example_user.name
+}
+
+resource "aws_iam_user_login_profile" "example_login_profile" {
+  user                  = aws_iam_user.example_user.name
+  password              = "SuperSecurePassword123!" # Replace with a strong password
+  password_reset_required = true # Forces the user to reset password on first login
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.example_access_key.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.example_access_key.secret
+  sensitive = true
+}
+
+output "console_login_url" {
+  value = "https://console.aws.amazon.com/"
+}
